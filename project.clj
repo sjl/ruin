0c20fc0336b7c3ad3c99d9ed4bc6a9f685462dc3
Steve Losh <steve@stevelosh.com>
2012-08-04 00:05:38 -0400

    Get a screen up.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject ruin "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :description ""
+  :url ""
+  :license {:name "MIT/X11"}
+  :main ^{:skip-aot true} ruin.core
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clojure-lanterna "0.9.2"]])
--- src/ruin/core.clj
+++ src/ruin/core.clj
@@ -1,6 +1,32 @@
-(ns ruin.core)
+(ns ruin.core
+  (:require [lanterna.screen :as s]))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+
+(defonce game (ref nil))
+
+
+(defn create-fresh-game []
+  (dosync
+    (ref-set game {})
+    (let [scr (s/get-screen)]
+      (alter game assoc :screen scr)
+      (s/start scr))))
+
+
+(defn run-game []
+  (s/stop (:screen @game)))
+
+(defn main []
+  (create-fresh-game)
+  (run-game))
+
+(defn -main [& args]
+  (main))
+
+
+(comment
+
+  (main)
+
+  )
+
